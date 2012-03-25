6fb18d4f86eee0f65d7605b8e32bb23e6f5974fa
Miro Ljubicic <miro@ljubicic.ch>
2012-03-25 23:46:35 +0200

    Rundung auf 5 Rappen implementiert :-)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: {greater_than_or_equal_to: 0.05} 
+  validates :price, format: { 
+    with: %r{(5|0)$},
+    message: 'must be rounded to 5 cents.' 
+  }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
