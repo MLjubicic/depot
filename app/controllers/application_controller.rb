68ae408ac6fb84f660474ecfd0571b567e6240b0
Miro Ljubicic <miro@ljubicic.ch>
2012-05-22 10:45:41 +0200

    ein paar kleine Typos mit Code aus dem Buch ausgebessert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-  
-    def current_cart
+
+    def current_cart 
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       cart = Cart.create
-      session[:cart_id] = cart_id
+      session[:cart_id] = cart.id
       cart
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-  has many :line_items
+  has_many :line_items
+
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
@@ -13,8 +14,19 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  
+    private
 
-
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
+end
 private
 
   def ensure_not_referenced_by_any_line_item
@@ -25,6 +37,3 @@ private
       return false
     end
   end
-
-
-end
