c1d17a879d252b394b2e710147583ea3e29d14e8
Miro Ljubicic <miro@ljubicic.ch>
2012-05-22 11:15:16 +0200

    Carts Controller (app/controllers/carts_controller.rb) mit rescue erweitert

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,16 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
     end
   end
 
@@ -61,7 +66,7 @@ class CartsController < ApplicationController
     respond_to do |format|
       if @cart.update_attributes(params[:cart])
         format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { head :no_content }
+        format.json { head :ok }
       else
         format.html { render action: "edit" }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
@@ -72,12 +77,13 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
-
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+      format.html { redirect_to store_url,
+        notice: 'Your cart is currently empty' }
+      format.json { head :ok }
     end
   end
 end
